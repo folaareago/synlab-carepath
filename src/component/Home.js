fbe3139d67d2a186bcf3dca3a415c2c70839bc2c
folaareago <folajanded@yahoo.com>
2021-06-26 17:11:09 +0100

    fixed

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,7 +8,7 @@ function Home() {
     
     const linkGen = async () => {
         let pdfName = localStorage.getItem('synlab-name');
-        setInputValue(`https://rv.synlab-pathcare.com/verify/synlab/${pdfName}`)
+        setInputValue(`https://rv.synlab-pathcare.com/verify/${pdfName}`)
         localStorage.removeItem('synlab-name')
     }
     const upload = async (e) => {
@@ -16,11 +16,10 @@ function Home() {
         let formData = new FormData();    //formdata object
         formData.append('pdf', inputForm);   //append the values with key, value pair
         const headers = { 'content-type': 'multipart/form-data', 'withCredentials': true, }
-        console.log(formData);
         axios
-          .post("https://synlab-carepath.herokuapp.com/upload", formData, { headers: headers})
+          .post("http://localhost:8080/upload", formData, { headers: headers})
           .then((response) => { 
-            if (response.status === "200") {
+            if (response.status === 200) {
               localStorage.setItem('synlab-name', response.data.name);
               alert('Uploaded Succesfully')
             } else {
@@ -36,7 +35,7 @@ function Home() {
                         <h1> UPLOAD SECTION</h1>
                     </div>
                     <form onSubmit={(e)=>{upload(e)}} id="formElem">   
-                        <input type="file" id="myFile" value={inputForm} onChange={e=>{setInputForm(e.target.value)}} className="uploadform" name="pdf" /> 
+                        <input type="file" id="myFile"  onChange={e=>{setInputForm(e.target.files[0]);}} className="uploadform" name="pdf" /> 
                         <div className="pad-top">  
                             <input type="submit" className="btn-edit" id="btnEdit" />
                             <button type= "button" onClick={linkGen} className="btn-edit" id="btnGen"> Generate Link </button>
